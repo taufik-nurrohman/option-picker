a16a67315412f0c1ea567af61b859e30b58e6f0b
Taufik Nurrohman <hi@taufik-nurrohman.com>
2025-05-20 21:05:57 +0700

    Fix Infinite Loop of `Set`

--- 2.2/index.js
+++ 2.2/index.js
@@ -293,13 +293,8 @@
             v = at.call(map, v, k);
             if (-1 === v) {
                 letValueInMap(k, map);
-                continue;
-            }
-            if (0 === v) {
+            } else if (0 === v) {
                 break;
-            }
-            if (1 === v) {
-                continue;
             }
             item = items.next();
         }
@@ -332,13 +327,8 @@
             v = at.call(set, v, k);
             if (-1 === v) {
                 letValueInMap(k, set);
-                continue;
-            }
-            if (0 === v) {
+            } else if (0 === v) {
                 break;
-            }
-            if (1 === v) {
-                continue;
             }
             item = items.next();
         }
